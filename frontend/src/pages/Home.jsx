05b1ec5fd338abe9cbc76167f74f72960f326b2a
Yugraj <yugrajjoshi185@gmail.com>
2026-05-13 00:08:50 +0530

    homepage design

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,10 +6,15 @@ function HomePage(){
     return(
     <>
     <main className="bg-black w-full h-screen">
+        <div classname =" w-half h-screen bg-black " >
+
+        </div>
 
     </main>
     
-    </>)
-};
+    </>
+    
+    );
+}
 
 export default HomePage;
--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -19,19 +19,19 @@ function LoginPage(){
             />
         </div>
         <div className="flex flex-col items-center  justify-center w-1/2 h-screen bg-white">
-             <div className="flex flex-col items-center justify-center w-[60%] bg-zinc-100  rounded-lg shadow-lg">
+             <div className="flex flex-col items-center justify-center w-[70%] bg-zinc-100  rounded-lg shadow-lg">
              <div className="flex flex-col items-center p-10 w-full h-full bg-zinc-300 rounded-lg shadow-lg">
                 <h1 className="text-zinc-700 font-extrabold text-4xl" >WELCOME!</h1>
                 <div className="flex flex-row rounded-4xl  w-full h-15 m-3 bg-gray-300 p-1 relative">
                     <div className={`absolute  h-15 w-[calc(50%-4px)] bg-white rounded-4xl transition-all duration-300 ${userType === "ADMIN" ? "left-1" : "left-[calc(50%+2px)]"}`}></div>
                     <button 
-                    className="w-full h-full text-gray-700 rounded-4xl pt-3 relative z-10 font-medium"
+                    className="w-full h-15  text-gray-700 rounded-4xl  relative z-10 font-medium"
                     onClick={() => handleUserTypeChange("ADMIN")}
                     >
                         Admin
                     </button>
                     <button 
-                    className="w-full h-full pt-3 text-gray-700 rounded-4xl relative z-10 font-medium"
+                    className="w-full h-15 text-gray-700 rounded-4xl relative z-10 font-medium"
                     onClick={() => handleUserTypeChange("USER")}
                     >
                         USER
@@ -39,7 +39,7 @@ function LoginPage(){
                 </div>
                 <div className=" w-full h-full p-5 rounded ">
                     <form className="flex flex-col items-center w-full h-full  gap-5">
-                        <label className="self-start text-gray-700 font-medium">Login as {userType}</label>
+                        <label className="self-start text-gray-700 font-medium ">Login as {userType}</label>
                         <label className="self-start -mb-3 ml-2 text-gray-700 font-medium">Username</label>
                         <input 
                         type="text" 
@@ -50,12 +50,18 @@ function LoginPage(){
                         type="password" 
                         placeholder="Password" 
                         className="w-full h-15 rounded-2xl focus:outline-none border bg-white border-gray-300 px-3"/>
-                        <br/>
+                        {userType === "USER" && <p className="text-gray-600 text-sm self-start ml-3 ">Use the credentials provided by your administrator</p>}
+                        {userType === "ADMIN" && <p className="text-gray-600 text-sm self-start ml-3 ">Use your admin credentials to login</p>}
                         <button 
                         type="submit" 
                         className="w-full h-15 bg-white transition-all duration-300 text-zinc-700 font-bold rounded-4xl hover:bg-gray-100">Login</button>
-
+                        <div className="flex flex-row justify-between w-full pl-5 pr-5  ">
+                            <button className="  rounded-4xl bg-white p-2 self-start "
+                            >language
+                            </button>
+                            
                         <a href="/forgot-password" className="  rounded-4xl self-end text-blue-500 hover:underline">Forgot Password?</a>
+                        </div>
                     </form>
                 </div>
              </div>
